62ca4c3a996ec8933eb5e8cb3d2dcb23bbf01460
Kirby McKenzie <kirby.mckenzie@cultureamp.com>
2022-08-01 09:26:24 +1000

    Add more hanami commands

--- src/hanami.ts
+++ src/hanami.ts
@@ -3,8 +3,68 @@ const completionSpec: Fig.Spec = {
   description: "",
   subcommands: [
     {
+      name: "console",
+      description: "Opens the Hanami Console",
+    },
+    {
       name: "db",
-      description: "Switch branches or restore working tree files",
+      description: "Database tools",
+      subcommands: [
+        {
+          name: "create",
+          description: "Create database",
+        },
+        {
+          name: "create_migration",
+          description: "Create new migration file",
+        },
+        {
+          name: "drop",
+          description: "Delete database",
+        },
+        {
+          name: "migrate",
+          description: "Migrates database",
+        },
+        {
+          name: "reset",
+          description: "Drop, create, and migrate databse",
+        },
+        {
+          name: "rollback",
+          description: "Rollback database to a previous migration",
+        },
+        {
+          name: "sample_data",
+          description: "Load sample data",
+        },
+        {
+          name: "seed",
+          description: "Load seed data",
+        },
+        {
+          name: "setup",
+          description: "Setup database",
+        },
+        {
+          name: "structure",
+          description: "TODO: Add structure description",
+          subcommands: [
+            {
+              name: "dump",
+              description: "Dumps database structure to db/structure.sql file",
+            },
+          ],
+        },
+        {
+          name: "version",
+          description: "Print schema version",
+        },
+      ],
+    },
+    {
+      name: "version",
+      description: "Prints the installed version of Hanami",
     },
   ],
   options: [
